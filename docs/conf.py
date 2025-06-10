6750f57e8024a40c117d954ca0df2fa99f2c8e61
Graham Morrison <graham.morrison@canonical.com>
2025-06-10 10:49:07 +0100

    added rediraffe extention to config

--- docs/conf.py
+++ docs/conf.py
@@ -252,6 +252,7 @@ extensions = [
     "sphinx_last_updated_by_git",
     "sphinx.ext.intersphinx",
     "sphinx_sitemap",
+    "sphinxext.rediraffe",
 ]
 
 # Excludes files or directories from processing
