54b56350424d8b21bb658c9ab86f2389ae1b1a4b
Graham Morrison <graham.morrison@canonical.com>
2025-07-17 14:17:51 +0100

    fixed new broken Ubuntu Core link.

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ html_context = {
     # TODO: If there's no such website,
     #       remove the {{ product_page }} link from the page header template
     #       (usually .sphinx/_templates/header.html; also, see README.rst).
-    "product_page": "https://ubuntu.com/core",
+    "product_page": "ubuntu.com/core",
     # Product tag image; the orange part of your logo, shown in the page header
     #
     # TODO: To add a tag image, uncomment and update as needed.
